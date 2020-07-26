13b917a17bcbbe732b4e77f493da25a72079a1fc
Stanislav <stanislav.kov.1996@gmail.com>
2020-07-26 10:46:50 +0300

    Added getting format type from MIME types in resource util

---
+++ socialnetwork-core/src/main/java/local/socialnetwork/core/type/MimeType.java
@@ -0,0 +1,29 @@
+package local.socialnetwork.core.type;
+
+public enum MimeType {
+
+    APPLICATION_JSON("application/json", "json"),
+    APPLICATION_PDF("application/pdf", "pdf"),
+    APPLICATION_XML("application/xml", "xml"),
+    IMAGE_GIF("image/gif", "gif"),
+    IMAGE_JPEG("image/jpeg", "jpg"),
+    IMAGE_PNG("image/png", "png"),
+    TEXT_HTML("text/html", "html"),
+    TEXT_CSS("text/css", "css");
+
+    private final String type;
+    private final String format;
+
+    MimeType(String type, String format) {
+        this.type = type;
+        this.format = format;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+}
