3e83e7167ee1b7b256028228293f6e85eb2d1ad1
Stanislav <mail@skov.dev>
2020-08-26 22:31:13 +0300

    Added logger in method change password in rest controller of user

--- socialnetwork-api/src/main/java/local/socialnetwork/api/controller/UserRestController.java
+++ socialnetwork-api/src/main/java/local/socialnetwork/api/controller/UserRestController.java
@@ -8,6 +8,8 @@ import local.socialnetwork.model.dto.ChangePasswordDto;
 
 import local.socialnetwork.model.http.ApiResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
@@ -22,6 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/user")
 public class UserRestController {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(UserRestController.class);
+
     private UserService userService;
 
     @Autowired
@@ -33,10 +37,13 @@ public class UserRestController {
     public ResponseEntity<ApiResponse> changePassword(@RequestBody ChangePasswordDto changePasswordDto) {
 
         if (!userService.checkIfValidOldPassword(changePasswordDto)) {
+            LOGGER.info("Password is not valid");
             throw new InvalidOldPasswordException("Old password is not valid");
         }
 
         userService.changePassword(changePasswordDto);
+
+        LOGGER.info("Password has updated successfully for {}", changePasswordDto.getUsername());
 
         return new ResponseEntity<>(new ApiResponse("Password has updated successfully"), HttpStatus.OK);
 
