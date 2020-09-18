5758ee0941e76009151aa8001d4b68b8bde9ec53
Stanislav <mail@skov.dev>
2020-09-19 00:15:26 +0300

    Added Spring Cloud Zuul for Api Gateway

--- socialnetwork-api-gateway/src/main/java/local/socialnetwork/apigateway/SpringBootRunApiGateway.java
+++ socialnetwork-api-gateway/src/main/java/local/socialnetwork/apigateway/SpringBootRunApiGateway.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class SpringBootRunApiGateway {
 
