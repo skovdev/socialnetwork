7c0058837d795a9c170c47ab3bb1f626fdf82ed0
Stanislav <mail@skov.dev>
2020-09-20 23:14:34 +0300

    Added security config with initializer

---
+++ socialnetwork-auth-service/src/main/java/local/socialnetwork/authservice/config/security/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package local.socialnetwork.authservice.config.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
