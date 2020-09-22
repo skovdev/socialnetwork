ad6c33b3ac551ff8630eccca452b147ac03993a5
Stanislav <mail@skov.dev>
2020-09-22 23:57:53 +0300

    Added auth rest controller in Auth Service

---
+++ socialnetwork-auth-service/src/main/java/local/socialnetwork/authservice/controller/AuthenticationRestController.java
@@ -0,0 +1,16 @@
+package local.socialnetwork.authservice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthenticationRestController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationRestController.class);
+
+
+
+}
