a513fa47e9c76258151e76c3ba82d20ee25654ee
Stanislav <mail@skov.dev>
2020-12-11 00:10:04 +0200

    Implementation UserConsumer in user service

--- socialnetwork-user-service/src/main/java/local/socialnetwork/userservice/kafka/consumer/user/UserConsumer.java
+++ socialnetwork-user-service/src/main/java/local/socialnetwork/userservice/kafka/consumer/user/UserConsumer.java
@@ -1,17 +1,36 @@
 package local.socialnetwork.userservice.kafka.consumer.user;
 
+import local.socialnetwork.userservice.service.UserService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.kafka.annotation.KafkaListener;
 
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class UserConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserConsumer.class);
 
-    public void receiveSomething() {
+    private static final String TOPIC_USER_DELETE = "topic.user.delete";
+    private static final String GROUP_USER_DELETE = "group.user.delete";
 
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @KafkaListener(topics = TOPIC_USER_DELETE, groupId = GROUP_USER_DELETE)
+    public void receiveUserIdForDeleteUser(UUID userId) {
+        LOGGER.info(String.format("### -> Received id of user: %s <- ###", userId));
+        userService.deleteById(userId);
     }
 }
