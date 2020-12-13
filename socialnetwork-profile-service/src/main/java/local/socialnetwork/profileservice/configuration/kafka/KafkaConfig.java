448471cce0eff515d2a7a60975eb1c1d711f72bb
Stanislav <mail@skov.dev>
2020-12-13 11:09:09 +0200

    Added common of kafka config to services of user and profile

---
+++ socialnetwork-profile-service/src/main/java/local/socialnetwork/profileservice/configuration/kafka/KafkaConfig.java
@@ -0,0 +1,109 @@
+package local.socialnetwork.profileservice.configuration.kafka;
+
+import local.socialnetwork.profileservice.model.kafka.Reply;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.kafka.annotation.EnableKafka;
+
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+
+    private static final String REPLY_TOPIC_COMMON = "reply.topic.common";
+
+    @Value("${spring.kafka.server}")
+    private String kafkaServer;
+
+    public Map<String, Object> producerConfig() {
+
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return config;
+
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    public Map<String, Object> consumerConfig() {
+
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "user-default-group-id");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
+        return props;
+
+    }
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ReplyingKafkaTemplate<String, Object, Reply> replyingKafkaTemplate(ProducerFactory<String, Object> producerFactory, KafkaMessageListenerContainer<String, Reply> listenerContainer) {
+        ReplyingKafkaTemplate<String, Object, Reply> replyReplyingKafkaTemplate = new ReplyingKafkaTemplate<>(producerFactory, listenerContainer);
+        replyReplyingKafkaTemplate.setSharedReplyTopic(true);
+        return replyReplyingKafkaTemplate;
+    }
+
+    @Bean
+    public KafkaMessageListenerContainer<String, Reply> replyListenerContainer() {
+        ContainerProperties containerProperties = new ContainerProperties(REPLY_TOPIC_COMMON);
+        return new KafkaMessageListenerContainer<>(consumerFactory(), containerProperties);
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setReplyTemplate(kafkaTemplate());
+        return factory;
+    }
+}
