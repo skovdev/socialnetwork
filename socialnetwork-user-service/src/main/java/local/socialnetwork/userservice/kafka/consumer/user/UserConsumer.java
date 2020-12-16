e714f8c42083488f4f36a05428157e372a9bf788
Stanislav <mail@skov.dev>
2020-12-16 13:42:29 +0200

    Added topic and kafka listener for receiving of user for updating to UserConsumer in user service

--- socialnetwork-user-service/src/main/java/local/socialnetwork/userservice/kafka/consumer/user/UserConsumer.java
+++ socialnetwork-user-service/src/main/java/local/socialnetwork/userservice/kafka/consumer/user/UserConsumer.java
@@ -1,5 +1,7 @@
 package local.socialnetwork.userservice.kafka.consumer.user;
 
+import local.socialnetwork.kafka.model.dto.profile.EditProfileDto;
+
 import local.socialnetwork.userservice.service.UserService;
 
 import org.slf4j.Logger;
@@ -18,14 +20,21 @@ public class UserConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserConsumer.class);
 
-    private static final String TOPIC_USER_DELETE = "topic.user.delete";
     private static final String USER_DEFAULT_GROUP_ID = "user-default-group-id";
+    private static final String TOPIC_USER_UPDATE = "topic.user.update";
+    private static final String TOPIC_USER_DELETE = "topic.user.delete";
 
     private UserService userService;
 
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    @KafkaListener(topics = TOPIC_USER_UPDATE, groupId = USER_DEFAULT_GROUP_ID)
+    public void receiveUserForUpdate(EditProfileDto editProfileDto) {
+        LOGGER.info(String.format("### -> Received fields of user for updating: %s", editProfileDto));
+        userService.update(editProfileDto);
     }
 
     @KafkaListener(topics = TOPIC_USER_DELETE, groupId = USER_DEFAULT_GROUP_ID)
