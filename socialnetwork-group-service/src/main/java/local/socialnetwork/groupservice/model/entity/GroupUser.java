ad04cbfe02738ce93909f1067187d3a7625afc98
Stanislav <mail@skov.dev>
2020-12-27 00:04:16 +0200

    Added new relationships entities for user and group in services of user and group. Moved GroupUser to new package in group service

--- socialnetwork-group-service/src/main/java/local/socialnetwork/groupservice/model/entity/GroupUser.java
+++ socialnetwork-group-service/src/main/java/local/socialnetwork/groupservice/model/entity/GroupUser.java
@@ -1,4 +1,4 @@
-package local.socialnetwork.groupservice.model.group;
+package local.socialnetwork.groupservice.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -6,9 +6,6 @@ import local.socialnetwork.groupservice.model.AbstractBaseModel;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import java.util.UUID;
@@ -17,12 +14,20 @@ import java.util.UUID;
 @Table(name = "sn_group_user")
 public class GroupUser extends AbstractBaseModel {
 
+    @Column(name = "group_id")
+    private UUID groupId;
+
     @Column(name = "user_id")
     private UUID userId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "group_id")
-    private Group group;
+    public UUID getGroupId() {
+        return groupId;
+    }
+
+    @JsonProperty(value = "groupId")
+    public void setGroupId(UUID groupId) {
+        this.groupId = groupId;
+    }
 
     public UUID getUserId() {
         return userId;
@@ -31,14 +36,5 @@ public class GroupUser extends AbstractBaseModel {
     @JsonProperty(value = "userId")
     public void setUserId(UUID userId) {
         this.userId = userId;
-    }
-
-    public Group getGroup() {
-        return group;
-    }
-
-    @JsonProperty(value = "group")
-    public void setGroup(Group group) {
-        this.group = group;
     }
 }
