0db4efd38c632647941cc46102f0300c39e2b37d
Stanislav <mail@skov.dev>
2021-03-20 17:36:39 +0200

    Refactored KafkaConfig in support service

--- socialnetwork-support-service/src/main/kotlin/local/socialnetwork/supportservice/config/kafka/KafkaConfig.kt
+++ socialnetwork-support-service/src/main/kotlin/local/socialnetwork/supportservice/config/kafka/KafkaConfig.kt
@@ -2,8 +2,8 @@ package local.socialnetwork.supportservice.config.kafka
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.common.serialization.StringDeserializer
 
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
 
 import org.springframework.beans.factory.annotation.Value
@@ -12,13 +12,19 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 import org.springframework.kafka.annotation.EnableKafka
+
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.config.KafkaListenerContainerFactory
-import org.springframework.kafka.core.*
+
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer
 
 import org.springframework.kafka.support.serializer.JsonDeserializer
-
 import org.springframework.kafka.support.serializer.JsonSerializer
 
 @EnableKafka
