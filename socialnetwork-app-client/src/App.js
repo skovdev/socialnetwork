92f9a420fd64c53e1d1b62d817285d187e07c3b4
Stanislav <mail@skov.dev>
2021-03-23 23:44:08 +0200

    Added component Support.js to App.js

--- socialnetwork-app-client/src/App.js
+++ socialnetwork-app-client/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
+
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import AuthenticationRoute from "./components/route/auth/AuthenticationRoute";
 
 import Main from "./components/main/Main";
+import AdminDashboard from "./components/admin/dashboard/AdminDashboard";
+import Support from "./components/support/Support";
 import Profile from "./components/profile/Profile";
 import EditProfile from "./components/profile/edit/EditProfile";
 import Group from "./components/profile/group/Group";
 import GroupList from "./components/profile/group/list/GroupList";
-import AdminDashboard from "./components/admin/dashboard/AdminDashboard";
 
 import "./App.css";
 
@@ -21,6 +23,7 @@ const App = () => {
                 <Switch>
                     <Route exact path="/" component={Main} />
                     <AuthenticationRoute path="/admin/dashboard" component={AdminDashboard} />
+                    <AuthenticationRoute path="/profile/support" component={Support} />
                     <AuthenticationRoute path="/profile/:username" exact component={Profile} />
                     <AuthenticationRoute path="/profile/:username/edit" component={EditProfile} />
                     <AuthenticationRoute path="/profile/:username/groups" component={GroupList} />
