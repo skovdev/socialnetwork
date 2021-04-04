f3c33ed88dc0b4ebdc5c0b465770928035c4e448
Stanislav <mail@skov.dev>
2021-04-04 11:35:44 +0300

    Added implementation to component Subject

--- socialnetwork-app-client/src/components/support/Support.js
+++ socialnetwork-app-client/src/components/support/Support.js
@@ -1,7 +1,51 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
+
+import AuthService from "../../service/auth/AuthService";
 
 const Support = () => {
 
+    const [subjects, setSubjects] = useState({});
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+
+        const getSubjects = AppConstants.API_HOST + "/api/v1/supports";
+        const token = AuthService.getToken();
+
+        axios.get(getSubjects, {
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+        }).then(response => {
+            setSubjects(response);
+            setIsLoaded(true);
+        }).catch(error => {
+            setIsLoaded(false);
+            setError(true);
+            console.log(error);
+        });
+    }, []);
+
+   if (error) {
+       return <div class="error">Error</div>
+   } else if (!isLoaded) {
+       return <div>Loading...</div>
+   } else {
+       return (
+           <div class="support">
+               {subjects.map(subject => {
+                   <div class="subject">
+                       <div>{subject.id}</div>
+                       <div>{subject.title}</div>
+                       <div>{subject.description}</div>
+                   </div>
+               })};
+           </div>
+       )
+   }
 
 }
 
