ef1f9da2dec435284ae8f5f2dbe27ec6be92baa3
Stanislav <mail@skov.dev>
2021-04-12 23:51:21 +0300

    Added ProfileCommandService in profile service

---
+++ socialnetwork-profile-service/src/main/java/local/socialnetwork/profileservice/service/impl/ProfileCommandService.java
@@ -0,0 +1,33 @@
+package local.socialnetwork.profileservice.service.impl;
+
+import local.socialnetwork.profileservice.commands.CreateProfileCommand;
+import local.socialnetwork.profileservice.model.dto.profile.ProfileDto;
+import local.socialnetwork.profileservice.service.ProfileCommandService;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class ProfileCommandServiceImpl implements ProfileCommandService {
+
+    private CommandGateway commandGateway;
+
+    @Autowired
+    public void setCommandGateway(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
+    public CompletableFuture<String> createProfile(ProfileDto profileDto) {
+
+        return this.commandGateway.send(new CreateProfileCommand(
+                profileDto.getId(),
+                profileDto.isActive(),
+                profileDto.getAvatar(),
+                profileDto.getUser().getId()));
+
+    }
+}
