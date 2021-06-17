327e9a8e1768d176b3a10e04ad18a13106d98396
Stanislav <mail@skov.dev>
2021-06-18 00:02:35 +0300

    Added UserDto, UserDetailsDto and RoleDto in user service

---
+++ socialnetwork-user-service/src/main/java/local/socialnetwork/userservice/model/dto/user/RoleDto.java
@@ -0,0 +1,34 @@
+package local.socialnetwork.userservice.model.dto.user;
+
+import java.util.UUID;
+
+public class RoleDto {
+
+    private UUID id;
+    private String authority;
+    private UserDto user;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public UserDto getUser() {
+        return user;
+    }
+
+    public void setUser(UserDto user) {
+        this.user = user;
+    }
+}
