776843a102aef31284f29695028e2bd2308418b6
Stanislav <mail@skov.dev>
2023-12-10 23:40:23 +0200

    Deleted unnecessary methods from ProfileRepository in the profile-service

--- socialnetwork-profile-service/src/main/java/local/socialnetwork/profileservice/repository/ProfileRepository.java
+++ socialnetwork-profile-service/src/main/java/local/socialnetwork/profileservice/repository/ProfileRepository.java
@@ -4,14 +4,11 @@ import local.socialnetwork.profileservice.model.entity.profile.Profile;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.data.repository.query.Param;
-
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, UUID> {
-    Profile findProfileById(@Param("id") UUID id);
-    Profile findProfileByAuthUserId(@Param("authUserId") UUID userId);
+
 }
