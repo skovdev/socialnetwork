4eca172cca0b2fa246825355942c87d74ffda4cd
Stanislav <mail@skov.dev>
2024-02-06 00:25:22 +0200

    [socialnetwork-profile-service]: Rewrote DTO classes in records

--- socialnetwork-profile-service/src/main/java/local/socialnetwork/profileservice/model/dto/profile/ProfileInfoDto.java
+++ socialnetwork-profile-service/src/main/java/local/socialnetwork/profileservice/model/dto/profile/ProfileInfoDto.java
@@ -1,29 +1,18 @@
 package local.socialnetwork.profileservice.model.dto.profile;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.AccessLevel;
-
-import lombok.experimental.FieldDefaults;
-
 import java.util.UUID;
 
-@Getter
-@Setter
-@ToString
-@FieldDefaults(level = AccessLevel.PRIVATE)
-public class ProfileInfoDto {
-    UUID id;
-    String firstName;
-    String lastName;
-    String country;
-    String city;
-    String address;
-    String phone;
-    String birthDay;
-    String familyStatus;
-    boolean isActive;
-    String avatar;
-    UUID userId;
-}
+public record ProfileInfoDto(
+        UUID id,
+        String firstName,
+        String lastName,
+        String country,
+        String city,
+        String address,
+        String phone,
+        String birthDay,
+        String familyStatus,
+        boolean isActive,
+        String avatar,
+        UUID userId
+) {}
