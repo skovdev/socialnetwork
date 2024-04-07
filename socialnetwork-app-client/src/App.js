81f1c900b0594465d22716ac54b678fee4b11b9e
Stanislav <mail@skov.dev>
2024-04-07 11:31:14 +0300

    [socialnetwork-app-client]: Refactored App.js component with styles

--- socialnetwork-app-client/src/App.js
+++ socialnetwork-app-client/src/App.js
@@ -1,31 +1,21 @@
 import React from "react";
 
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
-
-import AuthenticationRoute from "./components/route/auth/AuthenticationRoute";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import Main from "./components/main/Main";
-import AdminDashboard from "./components/admin/dashboard/AdminDashboard";
-import Profile from "./components/profile/Profile";
-import EditProfile from "./components/profile/edit/EditProfile";
 
 import "./App.css";
 
 const App = () => {
 
     return (
-
         <Router>
             <div className="app">
-                <Switch>
-                    <Route exact path="/" component={Main} />
-                    <AuthenticationRoute path="/admin/dashboard" component={AdminDashboard} />
-                    <AuthenticationRoute path="/profile/:username" exact component={Profile} />
-                    <AuthenticationRoute path="/profile/:username/edit" component={EditProfile} />
-                </Switch>
+            <Routes>
+                    <Route path="/" element={<Main />} />
+                </Routes>
             </div>
         </Router>
-
     );
 }
 
