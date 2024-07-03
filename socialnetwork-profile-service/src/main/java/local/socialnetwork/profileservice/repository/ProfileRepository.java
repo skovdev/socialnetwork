71f992127dcbf42e407eb5531a87fea8ac8fbe21
Stanislav <mail@skov.dev>
2024-07-03 23:17:10 +0300

    [socialnetwork-profile-service]: Extended the current token

--- socialnetwork-profile-service/src/main/java/local/socialnetwork/profileservice/repository/ProfileRepository.java
+++ socialnetwork-profile-service/src/main/java/local/socialnetwork/profileservice/repository/ProfileRepository.java
@@ -7,8 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+import java.util.Optional;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, UUID> {
-
+    Optional<Profile> findProfileByUserId(UUID userId);
 }
