a589be816e37d01428dd527e4ff9d2d181caa732
Stanislav <mail@skov.dev>
2025-10-19 12:17:07 +0300

    [socialnetwork]: Added run app class

---
+++ src/main/java/local/socialnetwork/SocialNetworkApp.java
@@ -0,0 +1,12 @@
+package local.socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkApp {
+    static void main(String[] args) {
+        SpringApplication.run(SocialNetworkApp.class, args);
+    }
+}
